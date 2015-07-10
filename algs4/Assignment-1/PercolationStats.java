47fa22c3fe37b100774384f153ebf8b6227cd6bd
bgeetika <geetikagarg07@gmail.com>
2015-07-09 22:45:01 -0700

    updated

--- algs4/Assignment-1/PercolationStats.java
+++ algs4/Assignment-1/PercolationStats.java
@@ -3,7 +3,7 @@ public class PercolationStats {
    {
        
    }
-   public double mean()                      // sample mean of percolation threshold
+   public double mean() // sample mean of percolation threshold
    public double stddev()                    // sample standard deviation of percolation threshold
    public double confidenceLo()              // low  endpoint of 95% confidence interval
    public double confidenceHi()              // high endpoint of 95% confidence interval
