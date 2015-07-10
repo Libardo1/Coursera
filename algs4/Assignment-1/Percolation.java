8132425e1b16218c5584e3d7640e1ba2de5fca74
bgeetika <geetikagarg07@gmail.com>
2015-07-09 19:23:11 -0700

    Updated code

--- algs4/Assignment-1/Percolation.java
+++ algs4/Assignment-1/Percolation.java
@@ -1,3 +1,4 @@
+
 public class Percolation {
     
     public WeightedQuickUnionUF matrix;
@@ -6,34 +7,56 @@ public class Percolation {
     
     public Percolation(int N){
         int n = N * N + 2;
-        width = N
-        this.matrix = WeightedQuickUnionUF(n)
-        states = new int[n]
+        width = N;
+        matrix = WeightedQuickUnionUF(n);
+        states = new int[n];
         for(int i = 0; i< N*N; i++){
               states[i] = 0;
             }
         states[N*N] = 1;
         states[N*N+1] = 1;
+      
     }// create N-by-N grid, with all sites blocked
     
-    public location(inti, int j){
-        return = (i-1)*width+(j-1);
+    public int location(int i, int j){
+        return (((i-1)*width) + (j-1) );
     }
-   public void open(int i, int j)// open site (row i, column j) if it is not open already
+    public void open(int i, int j){// open site (row i, column j) if it is not open already
     cell = location(i,j); 
-    if (states[cell]  == 1
+   if (states[cell]  == 1){
        return;
-    states[cell] = 1
-     
-       
-       
-       
-       
-       
-       
-   public boolean isOpen(int i, int j) // is site (row i, column j) open?
-            {
-        if (status[location(i,j)] == 1){
+   }
+    states[cell] = 1;
+    
+   //not top
+        if(i != 1){
+        matrix.union(location(i-1,j), cell);
+    }
+    //top row
+    else{
+        matrix.uniion(width*width,cell);
+    }
+    // not last
+    if (i != width){
+        matrix.union(location(i+1,j), cell);
+    }
+    if(i == width){
+       matrix.union(((location(width*width)+1),j),cell);
+    }
+    
+    //left corner
+    if (j != 1){
+        matrix.union(location(i, j-1),cell);
+    }
+    
+    //right corner
+    if (j != width){
+                matrix.union(location(i, j+1),cell);
+    }
+
+}
+   public boolean isOpen(int i, int j){
+        if (states[location(i,j)] == 1){
            return false;
         }
         else{
@@ -42,12 +65,14 @@ public class Percolation {
             
     }
             
-   public boolean isFull(int i, int j)     // is site (row i, column j) full?
-            return matrix.connected(width*width, location(i,j))
+   public boolean isFull(int i, int j){     // is site (row i, column j) full?
+            return matrix.connected(width*width, location(i,j));
+   }
             
             
-   public boolean percolates()             // does the system percolate?
-            return matrix.connected(width*width, width*width+1)
+   public boolean percolates(){            // does the system percolate?
+            return matrix.connected(width*width, width*width+1);
+   }
             
    public static void main(String[] args ){
        
