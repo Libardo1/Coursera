3647686648fda99dc82c82e5d524cc6b996b5a02
bgeetika <geetikagarg07@gmail.com>
2015-07-11 20:06:09 -0700

    FINAL SOLUTION.

--- algs4/Assignment-1/Percolation.java
+++ algs4/Assignment-1/Percolation.java
@@ -1,98 +1,106 @@
 
 public class Percolation {
     
-    public WeightedQuickUnionUF matrix;
-    public int [] states; 
-    public int width;
-    static int OPEN = 1;
-    static int BLOCK = 0;
-    int TOP;
-    int BOTTOM;
-    int UPPER_VIRTUAL;
-    int BOTTOM_VIRTUAL;
+    private static boolean  OPEN = true;
+    private static boolean BLOCK = false;
+    private  WeightedQuickUnionUF matrix;
+    private boolean  [] states; 
+    private int width;
     
-    public Percolation(int N){
+    private WeightedQuickUnionUF matrixCopy;
+
+    private int TOP;
+    private int BOTTOM;
+    private int UPPER_VIRTUAL;
+    private int BOTTOM_VIRTUAL;
+    
+    public Percolation(int N) {
+        if (N <= 0) throw new IllegalArgumentException();
         int n = N * N + 2;
         width = N;
         matrix = new WeightedQuickUnionUF(n);
-        states = new int[n];
-        for(int i = 0; i< N*N; i++){
+        matrixCopy = new WeightedQuickUnionUF(n);
+        states = new boolean[n];
+        for (int i = 0; i < N*N; i++) {
             states[i] = BLOCK;
         }
         states[N*N] = OPEN;
         states[N*N+1] = OPEN;
-        TOP=1;
-        BOTTOM=N; 
+        TOP = 1;
+        BOTTOM = N; 
         UPPER_VIRTUAL = N*N;
         BOTTOM_VIRTUAL = N*N+1;
         
-    }// create N-by-N grid, with all sites blocked
+    } // create N-by-N grid, with all sites blocked
     
-    public int location(int i, int j){
-        if (i <= 0 || i > width) throw new IndexOutOfBoundsException("row index i out of bounds");
-        return (((i-1)*width) + (j-1) );
+    private  int location(int i, int j) {
+        if (i <= 0 || i > width || j <= 0 || j > width) throw 
+            new IndexOutOfBoundsException("row index i out of bounds");
+        return (((i - 1)*width) + (j-1));
     }
-    public void open(int i, int j){// open site (row i, column j) if it is not open already
-        if (i <= 0 || i > width) throw new IndexOutOfBoundsException("row index i out of bounds");
-        int cell = location(i,j); 
-        if (states[cell]  == OPEN){
+    public void open(int i, int j) { 
+        // open site (row i, column j) if it is not open already
+        if (i <= 0 || i > width || j <= 0 || j > width) throw
+            new IndexOutOfBoundsException("row index i out of bounds");
+        int cell = location(i, j); 
+        if (states[cell]  == OPEN) {
             return;
         }
         states[cell] = OPEN;
         
         //not top
         if (i == TOP) {
-            matrix.union(UPPER_VIRTUAL,cell);
+            matrixCopy.union(UPPER_VIRTUAL, cell);
+            matrix.union(UPPER_VIRTUAL, cell);
         }
-        
-        else if((i != TOP) && isOpen(i-1,j)){
-            matrix.union(location(i-1,j), cell);
+        else if (isOpen(i-1, j)) {
+            matrixCopy.union(location(i-1, j), cell);
+            matrix.union(location(i-1, j), cell);
         }
         //top row
         
         // not last
-        if(i==BOTTOM){
-            matrix.union(BOTTOM_VIRTUAL,cell);
+        if (i == BOTTOM) {
+            matrix.union(BOTTOM_VIRTUAL, cell);
         }
-        else if ((i != BOTTOM)&& isOpen(i+1,j)){
-            matrix.union(location(i+1,j), cell);
+        else if (isOpen(i+1, j)) {
+            matrixCopy.union(location(i+1, j), cell);
+            matrix.union(location(i+1, j), cell);
         }
-        
-        
+                
         //left corner
-        if ((j != 1)&& isOpen(i,j-1)){
-            matrix.union(location(i, j-1),cell);
+        if ((j != 1) && isOpen(i, j-1)) {
+            matrixCopy.union(location(i, j-1), cell);
+            matrix.union(location(i, j-1), cell);
         }
         
         //right corner
-        if ((j != width)&& isOpen(i,j+1)){
-            matrix.union(location(i, j+1),cell);
+        if ((j != width) && isOpen(i, j+1)) {
+            matrix.union(location(i, j+1), cell);
+             matrixCopy.union(location(i, j+1), cell);
         }
-        
     }
-    public boolean isOpen(int i, int j){
-        if (i <= 0 || i > width) throw new IndexOutOfBoundsException("row index i out of bounds");
-        if (states[location(i,j)] == OPEN){
-            return true;
-        }
-        else{
-            return false;
-        }
+
+    public boolean isOpen(int i, int j) {
+        if (i <= 0 || i > width || j <= 0 || j > width) throw
+            new IndexOutOfBoundsException("row index i out of bounds");
+        return (states[location(i, j)] == OPEN);
         
     }
     
-    public boolean isFull(int i, int j){     // is site (row i, column j) full?
-        if (i <= 0 || i > width) throw new IndexOutOfBoundsException("row index i out of bounds");
-        return matrix.connected(UPPER_VIRTUAL, location(i,j));
+    public boolean isFull(int i, int j) {     // is site (row i, column j) full?
+        if (i <= 0 || i > width || j <= 0 || j > width) throw 
+            new IndexOutOfBoundsException("row index i out of bounds");
+        return matrixCopy.connected(UPPER_VIRTUAL, location(i, j));
     }
     
     
-    public boolean percolates(){            // does the system percolate?
+    public boolean percolates() {            // does the system percolate?
         return matrix.connected(UPPER_VIRTUAL, BOTTOM_VIRTUAL);
     }
     
-    public static void main(String[] args ){
+    public static void main(String[] args) { 
         
         
-    }// test client (optional)
+    } // test client (optional)
 }
--- algs4/Assignment-1/PercolationStats.java
+++ algs4/Assignment-1/PercolationStats.java
@@ -1,48 +1,50 @@
 public class PercolationStats {
-    double [] percolation_threshhold;
+    private double [] percolationThreshhold;
     
-    public PercolationStats(int N, int T)// perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T)
+    // perform T independent experiments on an N-by-N grid
     {
-        if((N <= 0) || (T <= 0)) throw new IllegalArgumentException();
-        percolation_threshhold =  new double[T];
-        for(int i = 0; i< T; i++){
+        if ((N <= 0) || (T <= 0)) throw new IllegalArgumentException();
+        percolationThreshhold =  new double[T];
+        for (int i = 0; i < T; i++) {
             int round = 0;
             Percolation perc = new Percolation(N);
-            while(!perc.percolates()){
-                int row = StdRandom.uniform(1,N+1);
-                int column = StdRandom.uniform(1,N+1);
+            while (!perc.percolates()) {
+                int row = StdRandom.uniform(1, N+1);
+                int column = StdRandom.uniform(1, N+1);
                 
-                if(!perc.isOpen(row,column)){
-                    perc.open(row,column);
+                if (!perc.isOpen(row, column)) {
+                    perc.open(row, column);
                     round++;
                 }      
             }
-            percolation_threshhold[i]  = ((double)round)/(N*N); 
+            percolationThreshhold[i]  = ((double) round)/(N*N); 
         }
     }
     public double mean()// sample mean of percolation threshold
     {
-        return StdStats.mean(percolation_threshhold);
-        
-        
+        return StdStats.mean(percolationThreshhold);
     }
-    public double stddev(){                  // sample standard deviation of percolation threshold
-        return StdStats.stddev(percolation_threshhold);
+    
+    public double stddev() {                 
+        // sample standard deviation of percolation threshold
+        return StdStats.stddev(percolationThreshhold);
     }
     public double confidenceLo()// low  endpoint of 95% confidence interval
     {
-        return mean() - ((1.96*stddev())/Math.sqrt(percolation_threshhold.length));
+        return mean() - ((1.96*stddev())/Math.sqrt(percolationThreshhold.length));
     }
     public double confidenceHi()     
     {
-        return mean() + ((1.96*stddev())/Math.sqrt(percolation_threshhold.length));
-    }// high endpoint of 95% confidence interval
+        return mean() + ((1.96*stddev())/Math.sqrt(percolationThreshhold.length));
+    } // high endpoint of 95% confidence interval
     
-    public static void main(String[] args){
-        PercolationStats ps=new PercolationStats(200,100);
+    public static void main(String[] args) {
+        PercolationStats ps = new PercolationStats(200, 100);
         
         StdOut.print("mean = "+ps.mean()+"\n");
         StdOut.print("std dev = "+ps.stddev()+"\n");
-        StdOut.print("95% confidence interval = "+ps.confidenceLo()+", "+ps.confidenceHi());
+        StdOut.print("95% confidence interval = "
+                         +ps.confidenceLo()+", "+ps.confidenceHi());
     }
 }
