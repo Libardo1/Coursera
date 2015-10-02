598eb12828c065a36cd8f46b4873f63010ae743a
bgeetika <geetikagarg07@gmail.com>
2015-10-01 22:43:44 -0700

    all code

--- algs4/Assignment-4/Board.java
+++ algs4/Assignment-4/Board.java
@@ -1,28 +1,25 @@
+import java.lang.Math;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
     private int[][] board;
     private int N;
-    private int[][] goalBoard;
+    
     
     public Board(int[][] blocks){
+        System.out.println("here, creating a board");
       // construct a board from an N-by-N array of blocks
       // (where blocks[i][j] = block in row i, column j)
           N = blocks.length; 
-         int[][] board = new int[N][N];
-          for (int i = 1; i < N; i++) {
+          board = new int[N][N];
+          for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-              board[i][i] = blocks[i][j];
+              this.board[i][j] = blocks[i][j];
             }
           }
-          int start = 1;
-            for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-              goalBoard[i][j] = start;
-              start++;
-              if (start == N - 1) {
-                break;
-              }
-            }
-          }
+          System.out.println("board created");
+          
           
     }
     
@@ -35,8 +32,11 @@ public class Board {
       // number of blocks out of place
        for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
+                if (board[i][j] == 0) {
+                    continue;
+                }
               int val = (i * N) + j + 1;
-              if (board[i][j] != val){
+              if (board[i][j] != val) {
                  count++;
               }
             }
@@ -44,18 +44,21 @@ public class Board {
        return count;
     }
     public int manhattan() {
-        int sum = 0;
+        int sum_t = 0;
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
+              if (board[i][j] == 0) {
+                    continue;
+                }
               int val = (i * N) + j + 1;
               if (board[i][j] != val){
                  int r = val/N + 1;
                  int c = val - r*N;
-                 sum += abs( abs(i-r) + abs(j -c))
+                 sum_t +=  (Math.abs(i - r) + Math.abs(j - c));
               }
             }
             }
-        return sum;
+        return sum_t;
    // sum of Manhattan distances between blocks and goal
         
     
@@ -64,6 +67,9 @@ public class Board {
       // is this board the goal board?
       for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
+                if (i == N-1 && j == N-1 && board[i][j] == 0) {
+                    continue;
+                }
               int val = (i * N) + j + 1;
               if (board[i][j] != val){
                   
@@ -78,10 +84,31 @@ public class Board {
     
     
     public Board twin()     {
-  // a board that is obtained by exchanging any pair of blocks
+    // a board that is obtained by exchanging any pair of blocks
+        int[][] newboard = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                {
+                    newboard[i][j] = board[i][j];
+                }
+            }
+        }
+              
+           int row = 0;
+           if (newboard[row][0] == 0 || newboard[row][1] == 0) {
+               row = 1;
+           }
+        int temp = newboard[row][0];
+        newboard[row][0] = newboard[row][1];
+        newboard[row][1] = temp;
+        
+        return new Board(newboard);
       
     }
     public boolean equals(Object y) {
+        if ( y== null) {
+            return false;
+        }
       
      if(y==this) return true;
      if(y==null) return false;
@@ -103,12 +130,49 @@ public class Board {
     
       
     public Iterable<Board> neighbors()  
-    {
-        // all neighboring boards
-      
+    { // all neighboring boards
+        int row = 0;
+        int col = 0;
+        
+         for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (board[i][j] == 0) {
+                    row = i;
+                    col = j;
+                    break;
+                }
+            }
+         }
+         
+          List<Board> list = new ArrayList<Board>();
+          
+          int[] row_vals = {1,-1,0,0};
+          int[] col_vals = {0,0,-1,+1};
+          for (int i = 0;i < 4; i++) {
+              int row_val = row + row_vals[i];
+              int col_val = col_vals[i] + col;
+              
+              if ((row_val > 0 && row_val < N) && (col_val > 0 && col_val < N)){
+                  int[][] newboard = new int[N][N];
+                  for (int k = 0; i < N; i++) {
+                      for (int j = 0; j < N; j++) {
+                          {
+                              newboard[k][j] = board[k][j];
+                          }
+                      }
+                  }
+                 
+                  newboard[row][col] = newboard[row_val][col_val];
+                  newboard[row_val][col_val] = 0;
+                  list.add(new Board(newboard));
+              }
+              
+          }
+          return list;
     }
      public String toString(){
-      string output = "";
+      String output = "";
+      
       // string representation of this board (in the output format specified below)
        for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
@@ -117,13 +181,18 @@ public class Board {
               }
               output += "\n";
             }
+       return output;
             }
 
-    } 
     
-    public static void main(String[] args) // unit tests (not graded)
+    
+    public static void main(String[] args) 
         
     {
+        int [][] b = {{1 , 2,  3 },{4 , 5,  6},  {7,  8,  0}};
+        Board bod = new Board(b);
+        Board bod1 = new Board(b);
+        System.out.println(bod.manhattan());
         
     }
 }
--- algs4/Assignment-4/Solver.java
+++ algs4/Assignment-4/Solver.java
@@ -1,7 +1,124 @@
+import java.util.List;
+
 public class Solver {
-    public Solver(Board initial)           // find a solution to the initial board (using the A* algorithm)
-    public boolean isSolvable()            // is the initial board solvable?
-    public int moves()                     // min number of moves to solve initial board; -1 if unsolvable
-    public Iterable<Board> solution()      // sequence of boards in a shortest solution; null if unsolvable
-    public static void main(String[] args) // solve a slider puzzle (given below)
+    private Board board;
+    private Board twinb;
+    private int move;
+    
+    private class Tuple { 
+        public int x; 
+        public Board board; 
+        public Tuple prev;
+        public int moves;
+        public Tuple( int x, Board board, Tuple prev, int moves) { 
+            this.x = x; 
+            board = board;
+            prev = prev;
+            moves = moves;
+  } 
+} 
+    
+    public Solver(Board initial)     
+    {      
+        board = initial;
+        twinb = board.twin();
+        move = 0;
+        
+        MinPQ<Tuple> q = new MinPQ<Tuple>( new Comparator<Tuple>() {
+              public int compare(Tuple tup1, Tuple tup2) {
+              return compare((tup1.x), (tup2.x));
+    }
+    });
+        MinPQ<Tuple> qTwin = new MinPQ<Tuple>( new Comparator<Tuple>() {
+              public int compare(Tuple tup1, Tuple tup2) {
+              return compare((tup1.x), (tup2.x));
+    }
+    });
+       
+       
+        
+    
+     
+        
+    }
+    public boolean isSolvable() {
+        if (this.solution == null) {
+            return false;
+        }
+        else {
+            return true;
+        }
+        
+        
+       // is the initial board solvable?
+    }
+    public int moves() {
+        if (isSolvable()) {
+            return this.move;
+        }
+        else{
+            return -1;
+        }
+        
+        // min number of moves to solve initial board; -1 if unsolvable
+    }
+    
+    private Iterable<Board> find(Tuple t) {
+        List<Board> list = new ArrayList<Board>();
+        Board b = t.board;
+        while ( b.equals(this.board)) {
+            list.add(t.board);
+            b = (t.prev).board ;
+        }
+        return list;        
+          
+    }
+    public Iterable<Board> solution()  {
+       // sequence of boards in a shortest solution; null if unsolvable
+        int prior = board.hamming();
+        Tuple t = new Tuple(prior, board, null, 0);
+        q.insert(t);
+        qTwin.insert(t);
+        flag = false;
+         // find a solution to the initial board (using the A* algorithm)
+        while ((!q.isEmpty() )  || (!qTwin.isEmpty())) {
+            Tuple currTuple = q.min();
+            Tuple currTupleTwin = qTwin.min();
+            if ((currTupleTwin.board).isGoal()) {
+                Iterable<Board> ans = find(currTupleTwin);
+                flag = true;
+                //this.move = currentTupleTwin.prior;
+                break;
+            }
+            if ((currTuple.board).isGoal()){
+                Iterable<Board> ans = find(currTupleTwin);
+                this.move = currentTuple.prior;
+                break;
+            }
+            for (Board b : (currTuple.board).neighbors()) {
+                if (!b.equals((currTuple.prev).board)) {
+                    prior = b.hamming() + ((currTuple.board).moves) ;
+                    q.insert(new Tuple(prior, b,currTuple, ((currTuple.board).moves) + 1));
+                }
+            }
+            for (Board bTwin : (currTupleTwin.board).neighbors()) {
+                if (!bTwin.equals((currTupleTwin.prev).board)) {
+                    prior = bTwin.hamming() + ((currTupleTwin.board).moves) ;
+                    qTwin.insert(new Tuple(prior, bTwin, currTupleTwin, ((currTupleTwin.board).moves) + 1));
+                }
+            }
+            
+        }
+        if (flag == true) {
+            return null;
+        }
+        else {
+            return ans;
+        }
+           
+    }
+    public static void main(String[] args)
+    {// solve a slider puzzle (given below)
+        
+    }
 }
